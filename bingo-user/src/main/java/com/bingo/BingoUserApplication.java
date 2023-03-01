e40c117c1f61744b89a2181ebe2222953d3f201d
xuzhibin <xuzhibin@lucidata.cn>
2023-03-01 16:09:05 +0800

    Bingo-Backend

---
+++ bingo-user/src/main/java/com/bingo/BingoUserApplication.java
@@ -0,0 +1,17 @@
+package com.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 徐志斌
+ * @CreateDate: 2023/3/1
+ * @description:
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class BingoUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BingoUserApplication.class);
+    }
+}
