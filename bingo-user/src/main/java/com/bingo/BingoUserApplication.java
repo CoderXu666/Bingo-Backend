618431dced0d12df51b9508bc4ae05f88ac6aed7
xuzhibin <xuzhibin@lucidata.cn>
2023-03-01 21:57:37 +0800

    空壳项目

--- bingo-user/src/main/java/com/bingo/BingoUserApplication.java
+++ bingo-user/src/main/java/com/bingo/BingoUserApplication.java
@@ -1,5 +1,6 @@
 package com.bingo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableDiscoveryClient
 @ComponentScan("com.bingo.*")
+@MapperScan("com.bingo.mapper")
 @SpringBootApplication
 public class BingoUserApplication {
     public static void main(String[] args) {
