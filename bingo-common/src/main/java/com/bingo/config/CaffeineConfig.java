ca23a909568cc2d1b4a1a52853f3a736a4775769
xuzhibin <xuzhibin@lucidata.cn>
2023-03-02 10:38:17 +0800

    项目搭建

--- bingo-common/src/main/java/com/bingo/config/CaffeineConfig.java
+++ bingo-common/src/main/java/com/bingo/config/CaffeineConfig.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
  * Caffeine配置类
  */
 @Configuration
-public class CacheConfig {
+public class CaffeineConfig {
     @Bean
     public Cache<String, Object> caffeineCache() {
         return Caffeine.newBuilder()
