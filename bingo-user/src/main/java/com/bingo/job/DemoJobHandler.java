8393f3565b1bd1b89bf623408c0bf8ecc7f4fa58
徐志斌 <1262254123@qq.com>
2023-05-09 22:51:16 +0800

    Bingo项目搭建

--- bingo-user/src/main/java/com/bingo/job/DemoJobHandler.java
+++ bingo-user/src/main/java/com/bingo/job/DemoJobHandler.java
@@ -9,7 +9,7 @@ import static com.xxl.job.core.biz.model.ReturnT.SUCCESS;
 @Component
 public class DemoJobHandler {
     @XxlJob("demoJobHandler")
-    public ReturnT<String> execute(String param) {
+    public ReturnT<String> execute() {
         System.out.println("XXL-JOB测试");
         return SUCCESS;
     }
