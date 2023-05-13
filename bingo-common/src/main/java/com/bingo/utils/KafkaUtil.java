fd7de0415b9c2659caae7464e6ca85570c4b334e
徐志斌 <1262254123@qq.com>
2023-05-13 22:03:17 +0800

    Bingo开源项目搭建

---
+++ bingo-common/src/main/java/com/bingo/utils/KafkaUtil.java
@@ -0,0 +1,57 @@
+package com.bingo.utils;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @Author 徐志斌
+ * @Date: 2023/5/13 21:45
+ * @Version 1.0
+ * @Description: Kafka工具类
+ */
+public class KafkaUtil {
+    private static String url = "101.42.13.186:9092";
+
+    public static void main(String[] args) throws Exception {
+        getTopic();
+    }
+
+    /**
+     * 创建Topic
+     */
+    public static void createTopic() {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, url);
+        try (AdminClient adminClient = AdminClient.create(props)) {
+            NewTopic newTopic = new NewTopic("my-topic", 1, (short) 1);
+            adminClient.createTopics(Collections.singleton(newTopic));
+        }
+    }
+
+    /**
+     * 查询Topic
+     */
+    public static void getTopic() throws ExecutionException, InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", url);
+        AdminClient adminClient = AdminClient.create(props);
+        ListTopicsResult topicsResult = adminClient.listTopics();
+        System.out.println(topicsResult.names().get());
+    }
+
+    /**
+     * 删除Topic
+     */
+    public static void deleteTopic() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", url);
+        AdminClient adminClient = AdminClient.create(props);
+        adminClient.deleteTopics(Collections.singleton("my_topic"));
+    }
+}
