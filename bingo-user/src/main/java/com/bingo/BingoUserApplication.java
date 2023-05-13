4157881231cc3c8ab38004b2a6a062f55fd70a94
徐志斌 <1262254123@qq.com>
2023-05-13 10:14:21 +0800

    Bingo开源项目搭建

--- bingo-user/src/main/java/com/bingo/BingoUserApplication.java
+++ bingo-user/src/main/java/com/bingo/BingoUserApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -15,7 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @EnableAsync
 @EnableCaching
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @ComponentScan("com.bingo.*")
 @MapperScan("com.bingo.mapper")
 @SpringBootApplication
