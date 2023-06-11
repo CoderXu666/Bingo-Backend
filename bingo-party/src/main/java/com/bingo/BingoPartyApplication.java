27018c2b371ccdb374a93da7a9a32fa38c98ef25
徐志斌 <1262254123@qq.com>
2023-06-11 21:03:54 +0800

    Bingo Code Commit-徐志斌

---
+++ bingo-party/src/main/java/com/bingo/BingoPartyApplication.java
@@ -0,0 +1,25 @@
+package com.bingo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Author: 徐志斌
+ * @CreateDate: 2023/3/4
+ * @description:
+ * @Version: 1.0
+ */
+@EnableFeignClients
+@ComponentScan("com.bingo.*")
+@MapperScan("com.bingo.mapper")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BingoPartyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BingoPartyApplication.class);
+    }
+}
