45fe8687df09791dcfa17aed82ab963f855d2197
徐志斌 <1262254123@qq.com>
2023-06-30 22:43:14 +0800

    Bingo Code Commit-徐志斌

---
+++ bingo-common/src/main/java/com/bingo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.bingo.handler;
+
+import com.bingo.enums.RespCodeEnum;
+import com.bingo.pojo.resp.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Author 徐志斌
+ * @Date: 2023/6/30 21:22
+ * @Version 1.0
+ * @Description: GlobalExceptionHandler
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * 兜底异常
+     */
+    @ExceptionHandler(Exception.class)
+    public R bindException(Exception e ) {
+        log.error("--------------------Exception异常：{}--------------------", e.getMessage());
+        return R.out(RespCodeEnum.FAIL, e.getMessage());
+    }
+}
