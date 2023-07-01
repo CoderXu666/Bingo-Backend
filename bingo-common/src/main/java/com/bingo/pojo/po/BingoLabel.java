2bf1b0a0f26e553b8025a977374975bab83bc09d
zzy <2772249478@qq.com>
2023-07-01 11:45:15 +0800

    zzy 提交代码

---
+++ bingo-common/src/main/java/com/bingo/pojo/po/BingoLabel.java
@@ -0,0 +1,58 @@
+package com.bingo.pojo.po;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 周英俊
+ * @since 2023-07-01
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("bingo_label")
+public class BingoLabel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 标签内容
+     */
+    @TableField("label_content")
+    private String labelContent;
+
+    /**
+     * 创建时间
+     */
+    @TableField("create_time")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @TableField("update_time")
+    private Date updateTime;
+
+    /**
+     * 删除标识
+     */
+    @TableField("deleted")
+    @TableLogic
+    private Boolean deleted;
+
+
+}
