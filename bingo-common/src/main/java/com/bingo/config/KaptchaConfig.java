13ab891cc5cd61aa1ef895985608375ee864f639
徐志斌 <1262254123@qq.com>
2023-07-20 22:10:10 +0800

    Bingo Code Commit-徐志斌

--- bingo-common/src/main/java/com/bingo/config/KaptchaConfig.java
+++ bingo-common/src/main/java/com/bingo/config/KaptchaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
+import java.util.Random;
 
 
 /**
@@ -20,16 +21,17 @@ public class KaptchaConfig {
     public DefaultKaptcha getDefaultKaptcha() {
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         Properties properties = new Properties();
-        properties.setProperty("kaptcha.border", "yes");
-        properties.setProperty("kaptcha.border.color", "105,179,90");
-        properties.setProperty("kaptcha.session.key", "code");
-        properties.setProperty("kaptcha.textproducer.font.color", "blue");
-        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅 黑");
-        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        properties.setProperty("kaptcha.border", "no");
+        properties.setProperty("kaptcha.textproducer.char.string", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
         properties.setProperty("kaptcha.textproducer.char.length", "4");
-        properties.setProperty("kaptcha.textproducer.char.space", "4");
-        properties.setProperty("kaptcha.image.width", "150");
-        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.names", "Arial, Courier, Georgia");
+        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,102,204");
+        properties.setProperty("kaptcha.textproducer.char.space", "3");
+        properties.setProperty("kaptcha.image.width", "84");
+        properties.setProperty("kaptcha.image.height", "41");
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
         Config config = new Config(properties);
         defaultKaptcha.setConfig(config);
         return defaultKaptcha;
