47e16c6e89b577ed138dd97fc8cfcc0d50d4abcb
徐志斌 <1262254123@qq.com>
2023-09-16 22:38:26 +0800

    Code Commit By 徐志斌

--- bingo-common/src/main/java/com/bingo/constant/MinioConstant.java
+++ bingo-common/src/main/java/com/bingo/constant/MinioConstant.java
@@ -3,10 +3,16 @@ package com.bingo.constant;
 /**
  * @Author: 徐志斌
  * @CreateTime: 2023-09-13  13:41
- * @Description: TODO
+ * @Description: 对象存储信息常量
  * @Version: 1.0
  */
 public class MinioConstant {
-    public static final String AVATAR_BUCKET = "avatar-bucket";
-    public static final String AUDIO_BUCKET = "avatar-bucket";
+    /**
+     * Bucket桶
+     */
+    public static final String USER_AVATAR_BUCKET = "user-avatar-bucket";
+    public static final String CHAT_VOICE_BUCKET = "chat-voice-bucket";
+    public static final String CHAT_PICTURE_BUCKET = "chat-picture-bucket";
+    public static final String CHAT_VIDEO_BUCKET = "chat-video-bucket";
+    public static final String CHAT_FILE_BUCKET = "chat-file-bucket";
 }
