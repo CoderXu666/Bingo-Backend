78d544cfd1d15ff38629dfd617c32104967505da
徐志斌 <1262254123@qq.com>
2023-09-21 15:09:06 +0800

    Code Commit By 徐志斌

--- bingo-user/src/main/java/com/bingo/service/impl/BingoUserServiceImpl.java
+++ bingo-user/src/main/java/com/bingo/service/impl/BingoUserServiceImpl.java
@@ -23,9 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -51,8 +48,6 @@ import java.util.concurrent.TimeUnit;
 public class BingoUserServiceImpl extends ServiceImpl<BingoUserMapper, BingoUser> implements BingoUserService {
     @Autowired
     private BingoUserStore userStore;
-    @Autowired
-    private AuthenticationManager authenticationManager;
     @Autowired
     private DefaultKaptcha defaultKaptcha;
     @Resource
@@ -212,24 +207,16 @@ public class BingoUserServiceImpl extends ServiceImpl<BingoUserMapper, BingoUser
         String captchaKey = userDTO.getCaptchaKey();
 
         // 判断验证码是否正确
-//        String captchaVal = (String) redisTemplate.opsForValue().get(captchaKey);
-//        if (!captcha.equalsIgnoreCase(captchaVal)) {
-//            throw new BingoException(null);
-//        }
+        String captchaVal = (String) redisTemplate.opsForValue().get(captchaKey);
+        if (!captcha.equalsIgnoreCase(captchaVal)) {
+            throw new BingoException(null);
+        }
 
         // 判断用户账号是否存在
         BingoUser userInfo = userStore.findByAccountId(accountId);
         if (ObjectUtils.isEmpty(userInfo)) {
             throw new BingoException(ResponseEnum.USER_NOT_EXIST);
         }
-
-        // 校验账户、密码是否正确（authenticate直接调用loadUserByUsername）
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountId, passWord);
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        if (Objects.isNull(authenticate)) {
-            throw new BingoException(ResponseEnum.PASSWORD_ERROR);
-        }
-
         // 生成Token
         String token = JWTUtil.generateToken(userInfo.getUid());
         if (StringUtils.isEmpty(token)) {
